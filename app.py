68e89116127b80578f7d69459f74031d939f19ac
VladShi <g3rc@yandex.ru>
2021-02-06 21:09:16 +0300

    adds file receive and geocoding

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_file
+from werkzeug.utils import secure_filename
+import pandas as pd
+from geopy.geocoders import ArcGIS
 
 app = Flask(__name__)
 
@@ -6,9 +9,22 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/success')
+@app.route('/', methods=['POST'])
 def success():
-    pass
+    if request.method == 'POST':
+        ufile = request.files['file']
+        # ufile.save(secure_filename("uploaded_" + ufile.filename))
+        df = pd.read_csv(ufile)
+        if 'Address' in df.columns or 'address' in df.columns:
+            if 'address' in df.columns and 'Address' not in df.columns:
+                address = 'address'
+            else:
+                address = 'Address'
+            df['Latitude'] = df[address].apply(ArcGIS().geocode).apply(lambda x: x.latitude if x != None else None) 
+            df['Longitude'] = df[address].apply(ArcGIS().geocode).apply(lambda x: x.longitude if x != None else None)
+            return render_template("index.html", btn="download.html")
+        else:
+            pass  # notification about file has not "address" column 
 
 if __name__ == "__main__":
     app.debug=True
