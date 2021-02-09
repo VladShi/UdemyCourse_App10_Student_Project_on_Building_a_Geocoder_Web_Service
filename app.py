b898fdab0a9bcfa000624ac190f1e9b55a565885
VladShi <g3rc@yandex.ru>
2021-02-09 17:52:17 +0300

    add table output, save and send file

--- app.py
+++ app.py
@@ -5,12 +5,16 @@ from geopy.geocoders import ArcGIS
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 @app.route('/', methods=['POST'])
 def success():
+    global ufile
+    global df
     if request.method == 'POST':
         ufile = request.files['file']
         # ufile.save(secure_filename("uploaded_" + ufile.filename))
@@ -20,12 +24,26 @@ def success():
                 address = 'address'
             else:
                 address = 'Address'
-            df['Latitude'] = df[address].apply(ArcGIS().geocode).apply(lambda x: x.latitude if x != None else None) 
-            df['Longitude'] = df[address].apply(ArcGIS().geocode).apply(lambda x: x.longitude if x != None else None)
-            return render_template("index.html", btn="download.html")
+            df['Latitude'] = df[address].apply(ArcGIS().geocode).apply(
+                lambda x: x.latitude if x != None else None)
+            df['Longitude'] = df[address].apply(ArcGIS().geocode).apply(
+                lambda x: x.longitude if x != None else None)
+            return render_template("index.html",
+                                   tables=[
+                                       df.to_html(classes='data',
+                                                  index=False,
+                                                  header="true")
+                                   ],
+                                   btn="download.html")
         else:
-            pass  # notification about file has not "address" column 
+            pass  # notification about file has not "address" column
+
+
+@app.route('/download')
+def download():
+    df.to_csv(secure_filename("uploaded_" + ufile.filename), index=False)
+    return send_file("uploaded_" + ufile.filename, attachment_filename="yourfile.csv", as_attachment=True)
 
 if __name__ == "__main__":
-    app.debug=True
+    app.debug = True
     app.run(port=5001)
